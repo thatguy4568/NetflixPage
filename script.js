ef31c1558a2f749da80197478bd3eeb5dd9f8a16
Talisha Rahman <talisharahman3@gmail.com>
2025-06-16 14:50:54 -0400

    Initial Netflix layout

---
+++ script.js
@@ -0,0 +1,6 @@
+// No functionality required, but here’s a placeholder
+document.querySelector('.email-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thanks for entering your email!');
+  });
+  
